bcd3136f58ef38b119c67ab64bbe438c8c7ba7d6
Diego Alencar Jacober <57761933898>
2023-09-25 14:26:15 -0300

    Finalizando

--- posts/core/views.py
+++ posts/core/views.py
@@ -2,14 +2,25 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Post
 from .serializers import PostSerializer
+import requests
 
 
 class PostApiView(APIView):
     def get(self, request):
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
-        
+        return Response([self.formatPost(p) for p in posts])
+
+
+    def formatPost(self, post):
+        comments = requests.get(f'http://127.0.0.1:8001/api/posts/{post.id}/comments').json()
+
+        return {
+            'id': post.id,
+            'title': post.title,
+            'description': post.description,
+            'comments': comments
+        }
         
     def post(self, request):
         serializer = PostSerializer(data=request.data)
